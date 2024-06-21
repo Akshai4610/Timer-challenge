d4f4c7249eef8a0fdb373d6028f03e3cabd13bd0
Akshai P R <akshai4610@gmail.com>
2024-06-21 20:58:44 +0530

    Result,Timer component updation 1

--- src/components/ResultLog.jsx
+++ src/components/ResultLog.jsx
@@ -1,12 +1,20 @@
-export default function ResultLog({result,targetTime}){
-    return (
-        <dialog className="result-modal" open>
-            <h2>Your Score :{result} </h2>
-            <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>second left.</strong></p>
-            <form method="dialog">
-                <button>Close</button>
-            </form>
-        </dialog>
-    )
-}
+import { forwardRef } from "react";
+
+const ResultLog = forwardRef(function ResultLog({ result, targetTime }, ref) {
+  return (
+    <dialog ref={ref} className="result-modal">
+      <h2>Your {result} </h2>
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with <strong>second left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
+
+export default ResultLog;
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,38 +1,42 @@
-import { useState, useRef } from "react"
-import ResultLog from "./ResultLog"
+import { useState, useRef } from "react";
+import ResultLog from "./ResultLog";
 
-export default function TimerChallenge({title,targetedTime}){
+export default function TimerChallenge({ title, targetedTime }) {
+  const timer = useRef();
+  const dialog = useRef();
 
-    const timer=useRef()
-    const[timeStarted,setTime]=useState(false)
-    const[timeExpired,setTimeExpire]=useState(false)
+  const [timeStarted, setTime] = useState(false);
+  const [timeExpired, setTimeExpire] = useState(false);
 
-    function startTime(){
-        setTime(true)
-        timer.current=setTimeout(() => {
-           setTimeExpire(true) 
-        }, targetedTime * 1000);
-    }
+  function startTime() {
+    timer.current = setTimeout(() => {
+      setTimeExpire(true);
+      dialog.current.showModal();
+    }, targetedTime * 1000);
+    setTime(true);
+  }
 
-    function stopTime(){
-        clearTimeout(timer.current)
-    }
+  function stopTime() {
+    clearTimeout(timer.current);
+  }
 
-    return(
-        <>
-        <section className="challenge">
+  return (
+    <>
+      <ResultLog ref={dialog} targetTime={targetedTime} result="Lost" />
+      <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
-            {targetedTime} second {targetedTime>1 ? 's' : ' '}
+          {targetedTime} second {targetedTime > 1 ? "s" : " "}
         </p>
         <p>
-            <button onClick={timeStarted ? stopTime :startTime}>{timeStarted ? "stop" : "Start"} Challenge</button>
+          <button onClick={timeStarted ? stopTime : startTime}>
+            {timeStarted? "stop" : "Start"} Challenge
+          </button>
         </p>
-        <p className={timeStarted ? 'active' :undefined}>
-            {timeStarted ? "Timer running......" : "Timer  Inactive"}
+        <p className={timeStarted ? "active" : undefined}>
+          {timeStarted ? "Timer running......" : "Timer  Inactive"}
         </p>
-        </section>
-        {timeExpired && <ResultLog targetTime={targetedTime} result='Lost'/>}
-        </>
-    )
+      </section>
+    </>
+  );
 }
