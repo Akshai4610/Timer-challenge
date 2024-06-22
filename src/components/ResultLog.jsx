fb3ab7a7c371194b73edce8f7e47ce015fd6c6bb
Akshai P R <akshai4610@gmail.com>
2024-06-22 15:08:04 +0530

    Bug fixed,replaced the position of dialog box

--- src/components/ResultLog.jsx
+++ src/components/ResultLog.jsx
@@ -1,34 +1,38 @@
-import { forwardRef, useImperativeHandle, useRef} from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom"; //used to change the position of this component(teleport)
 
-const ResultLog = forwardRef(function ResultLog({ result, targetTime, timeRemaining, timeComplete}, ref) {
-  
-  const gameResult = (timeRemaining / 1000).toFixed(2)
-    const userLost= timeRemaining<=0
-    const score=Math.round((1-timeRemaining / (targetTime * 1000))*100)
-    const dialog=useRef();
-  
-    useImperativeHandle(ref, ()=>{
-        return {
-            open(){
-                    dialog.current.showModal()
-            }
-        }
-    })
+const ResultLog = forwardRef(function ResultLog(
+  { result, targetTime, timeRemaining, timeComplete },
+  ref
+) {
+  const gameResult = (timeRemaining / 1000).toFixed(2);
+  const userLost = timeRemaining <= 0;
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+  const dialog = useRef();
 
-    return (
-    <dialog ref={dialog} className="result-modal">
-      {userLost && <h2>You Lost The Challenge</h2> }
-      {!userLost && <h2>Your Score : {score}</h2> }
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal( //this part is need to teleport having 2 arrg. onClose is used to disable the (Esc) closing
+    <dialog ref={dialog} className="result-modal" onClose={onreset}>
+      {userLost && <h2>You Lost The Challenge</h2>}
+      {!userLost && <h2>Your Score : {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-         You stopped the timer with <strong>{gameResult} second left.</strong>
+        You stopped the timer with <strong>{gameResult} second left.</strong>
       </p>
       <form method="dialog" onSubmit={timeComplete}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   );
 });
 
